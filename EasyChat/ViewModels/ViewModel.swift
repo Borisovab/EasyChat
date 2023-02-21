c9f2262dddf81e72268e7fcd9b711d97e8ac6ea3
Alexander_Borisov <borisovab@mail.ru>
2023-02-21 14:19:37 +0300

    logVCDone

--- EasyChat/ViewModels/ViewModel.swift
+++ EasyChat/ViewModels/ViewModel.swift
@@ -18,7 +18,7 @@ class ViewModel {
             statusColor.value = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
         } else {
             statusText.value = "You successfully logged in"
-            statusColor.value = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+            statusColor.value = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
         }
     }
 }
