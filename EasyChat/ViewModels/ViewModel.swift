4661be7bf66b0b9c09a0a963be3cf1cfcd11f696
Alexander_Borisov <borisovab@mail.ru>
2023-02-21 14:20:54 +0300

    LoginVCDone

--- EasyChat/ViewModels/ViewModel.swift
+++ EasyChat/ViewModels/ViewModel.swift
@@ -15,7 +15,7 @@ class ViewModel {
     func userButtonPressed(login: String, password: String) {
         if login != User.logins.first?.login || password != User.logins.first?.password {
             statusText.value = "Login failed"
-            statusColor.value = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+            statusColor.value = #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1)
         } else {
             statusText.value = "You successfully logged in"
             statusColor.value = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
